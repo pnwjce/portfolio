48fb4bf6ea7a8bcdd44922275e82c38ebbe97c98
pnwjce <elliott.jess@gmail.com>
2018-12-01 12:05:51 -0800

    scaffolding

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Node Modules
+//dependencies
+
+require('dotenv').config();
+const express = require('express');
+
+//app
+const app = express();
+
+//PORT
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
+})
+
+
+app.use('*', (req, res) => {
+  res.status(400).send('sorry that didnt work');
+})
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
